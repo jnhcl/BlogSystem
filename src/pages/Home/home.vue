9a0f68f12079d6d71cceab5ee06ef457f445a726
周小云 <ironknt@163.com>
2018-06-20 17:29:20 +0800

    应用element-ui

---
+++ src/pages/Home/home.vue
@@ -0,0 +1,31 @@
+<style scpoed>
+  .home-page{
+    display: flex;
+    flex-direction: column;
+    height: auto;
+  }
+</style>
+
+<template>
+  <div class="home-page">
+    <top-menu/>
+    <sub-menu/>
+  </div>
+</template>
+
+<script>
+import topMenu from '@/common/head.vue'
+import subMenu from '@/common/sub.vue'
+export default {
+  name: 'HomePage',
+  data () {
+    return {
+      msg: 'Hello CatBlog'
+    }
+  },
+  components: {
+    topMenu,
+    subMenu
+  }
+}
+</script>
